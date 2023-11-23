7319ef5dc83f1ec88597a165fe2c44aea0b15790
Anton Bringner <antbri1009@skola.goteborg.se>
2023-11-23 13:23:42 +0100

    fixing script

--- run_game.sh
+++ run_game.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+mkdir anton_bringner_labb
+cp *.java anton_bringner_labb/
+cd /anton_bringner_labb#!/bin/bash
 
 echo "Anton_Bringners program"
 mkdir anton_bringner_labb
-cp -r guessingGame anton_bringner_labb
-cd yrgo/temp/git/inlämningsuppgift1/anton_bringner_labb
+cp *.java anton_bringner_labb/
+cd anton_bringner_labb
 pwd
-echo "Running game from /home/yrgo/temp/git/inlämningsuppgift1/anton_bringner_labb"
+echo "Running game from" $(pwd)
 echo "Compiling game..."
 javac GuessingGame.java
 echo "Running game..."
